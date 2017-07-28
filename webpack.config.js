f6656c5e2dfe7bf85b33486b8269e1761155cfb6
dahlki <dahlki78@gmail.com>
2017-07-28 11:22:40 -0400

    setup project with webpack, http server, and initial paperjs animation

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: './client/bloomingice.js',
+  output: {
+    path: __dirname + '/public/dist', // the absolute path for the directory where we want the output to be placed
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015']
+        }
+      },
+      // {
+      //   test: /\.scss$/,
+      //   use: [
+      //     'style-loader',
+      //     'css-loader',
+      //     'sass-loader'
+      //   ]
+      // }
+    ]
+  }
+};
